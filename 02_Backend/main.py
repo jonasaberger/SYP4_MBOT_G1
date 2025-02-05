56dc323c72604c90e4f4f6c3199a61b95e94651b
Tim Hechenberger <timhec44@gmail.com>
2025-02-05 10:39:40 +0100

    Refactor MBotBridge to integrate Flask for command handling and status retrieval; update main to start server

--- 02_Backend/main.py
+++ 02_Backend/main.py
@@ -3,11 +3,9 @@ import mbot_bridge as mbb
 def main(): 
     print('MBOT-G1 Backend')
     mbot_bridge = mbb.MBotBridge()
-
-
-    mbot_bridge.configure_connection('10.10.3.188')
-    mbot_bridge.send_message('discovery')
-
+    #mbot_bridge.configure_connection('10.10.3.188')
+    #mbot_bridge.send_message('discovery')
+    mbot_bridge.start_server()
 
 
 main()
