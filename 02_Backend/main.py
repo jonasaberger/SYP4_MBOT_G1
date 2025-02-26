99a6b3ce2a0a11296e33c6218a9731b34527a5cb
jonasaberger <145124362+jonasaberger@users.noreply.github.com>
2025-02-26 08:05:54 +0100

    Add documentation for /receive_commands endpoint in main.py

--- 02_Backend/main.py
+++ 02_Backend/main.py
@@ -1,5 +1,16 @@
 import frontend_bridge as feb
 
+# ENDPOINTS : /receive_commands
+# POST:
+# 1. ip-target : string -> IP address of the target mbot
+#    |
+# 2. mode : string -> mode of the mbot (e.g. "controller", "discovery") | exit the mode by sending "exit"
+#    |
+# 3. drive : string -> drive command (e.g. "forward", "backward", "left", "right", "stop")
+# 4. color : string -> color of the mbot (e.g. "255,255,255")
+# 5. speed : string -> speed of the mbot (e.g. "1", "100")
+
+
 def main(): 
     print('MBOT-G1 Backend')
     frontend_bridge = feb.FrontendBridge()
